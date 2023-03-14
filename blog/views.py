3943364eb65641ec0490ed7d489532b49d401be4
srAndrii <55akif5548@gmail.com>
2023-03-14 17:58:04 +0200

    Outputting Comments

--- blog/views.py
+++ blog/views.py
@@ -35,7 +35,8 @@ class SinglePostView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": CommentForm()
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id")
         }
         return render(request, "blog/post-detail.html", context)
 
@@ -53,7 +54,8 @@ class SinglePostView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": comment_form
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id")
         }
         return render(request, "blog/post-detail.html", context)
     # template_name = "blog/post-detail.html"
