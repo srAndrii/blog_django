1598b31fdf9f4975a31496fa25aa18e36ed79931
srAndrii <55akif5548@gmail.com>
2023-03-19 08:43:13 +0200

    Deployment.Serving Static Files

--- blog_django/urls.py
+++ blog_django/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls'))
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
